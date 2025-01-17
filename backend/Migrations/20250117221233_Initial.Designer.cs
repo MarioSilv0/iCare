87a41ec75f332ba3c7016b4aee2efd7e3746b6ce
Luís Martins <202100239@estudantes.ips.pt>
2025-01-17 23:04:37 +0000

    CSS Fixes

--- backend/Migrations/20250117221233_Initial.Designer.cs
+++ backend/Migrations/20250117221233_Initial.Designer.cs
@@ -12,7 +12,7 @@ using backend.Data;
 namespace backend.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20250117102442_Initial")]
+    [Migration("20250117221233_Initial")]
     partial class Initial
     {
         /// <inheritdoc />
