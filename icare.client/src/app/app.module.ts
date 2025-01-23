e2f43df8d07ff61e092e0881b16604deffbd9a76
João Pedro <irus0ubusiness@gmail.com>
2025-01-23 16:10:36 +0000

    feat: recover-password

--- icare.client/src/app/app.module.ts
+++ icare.client/src/app/app.module.ts
@@ -11,6 +11,7 @@ import { HomeComponent } from './home/home.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
+import { RecoverPasswordComponent } from './auth/recover-password/recover-password.component';
 
 @NgModule({
   declarations: [
@@ -21,6 +22,7 @@ import { RegisterComponent } from './auth/register/register.component';
     NavMenuComponent,
     LoginComponent,
     RegisterComponent,
+    RecoverPasswordComponent,
   ],
   imports: [
     BrowserModule, HttpClientModule,
--- icare.client/src/app/auth/auth.service.ts
+++ icare.client/src/app/auth/auth.service.ts
@@ -1,20 +1,21 @@
-import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 
-  //Mário
+//Mário
 export class AuthService {
   private baseUrl = '../api/account';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   isAuthenticated(): Observable<boolean> {
-    return this.http.get<boolean>(`${this.baseUrl}/isAuthenticated`, { withCredentials: true, }
-    );
+    return this.http.get<boolean>(`${this.baseUrl}/isAuthenticated`, {
+      withCredentials: true,
+    });
   }
 
   login(credentials: { email: string; password: string }): Observable<any> {
@@ -24,14 +25,21 @@ export class AuthService {
   }
 
   logout(): Observable<any> {
-    return this.http.post(`${this.baseUrl}/logout`, {}, {
-      withCredentials: true,
-    });
+    return this.http.post(
+      `${this.baseUrl}/logout`,
+      {},
+      {
+        withCredentials: true,
+      }
+    );
   }
 
   register(data: { email: string; password: string }): Observable<any> {
     return this.http.post(`${this.baseUrl}/register`, data);
   }
 
-
+  recover(email: string): Observable<any> {
+    //? verificar que tipo de metodo é
+    return this.http.post(`${this.baseUrl}/recover-password`, email);
+  }
 }
