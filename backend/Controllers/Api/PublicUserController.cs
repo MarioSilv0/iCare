5eafd83e5742ca113e9e0904c8f5d82c94b2b2d0
Luís Martins <202100239@estudantes.ips.pt>
2025-01-26 17:17:55 +0000

    Refactor
    Beginning the refactor of the profile implementation

--- backend/Controllers/Api/PublicUserController.cs
+++ backend/Controllers/Api/PublicUserController.cs
@@ -2,11 +2,8 @@
 using backend.Models;
 using backend.Models.Preferences;
 using backend.Models.Restrictions;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
 
 namespace backend.Controllers.Api
 {
@@ -26,61 +23,27 @@ namespace backend.Controllers.Api
         {
             var user = await _context.Users.Include(u => u.UserPreferences)
                                            .ThenInclude(up => up.Preference)
-                                           .Include(u => u.UserRestrictions) 
+                                           .Include(u => u.UserRestrictions)
                                            .ThenInclude(ur => ur.Restriction)
                                            .FirstOrDefaultAsync(u => u.Id == id);
 
-            if (user == null)
-            {
-                return NotFound();
-            }
+            if (user == null) return NotFound();
 
-            var preferences = await _context.Preferences.ToListAsync();
-            var restrictions = await _context.Restrictions.ToListAsync();
-            var publicUser = new PublicUser
-            {
-                Picture = user.Picture,
-                Name = user.Name,
-                Email = user.Email,
-                Birthdate = user.Birthdate,
-                Notifications = user.Notifications,
-                Height = user.Height,
-                Weight = user.Weight,
-                Preferences = preferences.Select(p => new SelectionObject
-                {
-                    Id = p.Id,
-                    Name = p.Name,
-                    IsSelected = user.UserPreferences.Any(up => up.PreferenceId == p.Id),
-                }).ToList(),
-                Restrictions = restrictions.Select(r => new SelectionObject
-                {
-                    Id = r.Id,
-                    Name = r.Name,
-                    IsSelected = user.UserRestrictions.Any(ur => ur.RestrictionId == r.Id),
-                }).ToList(),
-            };
-
-            return publicUser;
+            return await PublicUser.CreatePublicUser(user, null, _context.Preferences.ToListAsync(), _context.Restrictions.ToListAsync());
         }
 
         [HttpPut("{id}")]
         public async Task<ActionResult<PublicUser>> Edit(string id, [FromBody] PublicUser model)
         {
-            if (model == null || id != model.Id)
-            {
-                return BadRequest();
-            }
+            if (model == null || id != model.Id) return BadRequest();
 
             var user = await _context.Users.Include(u => u.UserPreferences)
                                            .Include(u => u.UserRestrictions)
                                            .FirstOrDefaultAsync(u => u.Id == id);
 
-            if (user == null)
-            {
-                return NotFound();
-            }
+            if (user == null) return NotFound();
 
-            user.Picture = string.IsNullOrEmpty(model.Picture)? user.Picture : model.Picture;
+            user.Picture = string.IsNullOrEmpty(model.Picture) ? user.Picture : model.Picture;
             user.Name = string.IsNullOrEmpty(model.Name) ? user.Name : model.Name;
             user.Email = string.IsNullOrEmpty(model.Email) ? user.Email : model.Email;
             user.Birthdate = model.Birthdate;
@@ -88,47 +51,26 @@ namespace backend.Controllers.Api
             user.Height = model.Height <= 0 || model.Height > 3 ? user.Height : model.Height;
             user.Weight = model.Weight <= 0 || model.Weight > 700 ? user.Weight : model.Weight;
 
-            user.UserPreferences.Clear();
-            foreach(SelectionObject p in model.Preferences)
-            {
-                if (!p.IsSelected) continue;
-
-                user.UserPreferences.Add(new UserPreference
-                {
-                    UserId = user.Id,
-                    PreferenceId = p.Id
-                });
-            }
-
-            user.UserRestrictions.Clear();
-            foreach (SelectionObject r in model.Restrictions)
-            {
-                if (!r.IsSelected) continue;
-
-                user.UserRestrictions.Add(new UserRestriction
-                {
-                    UserId = user.Id,
-                    RestrictionId = r.Id
-                });
-            }
+            UpdateCollection(user.UserPreferences, model.Preferences, e => new UserPreference { UserId = user.Id, PreferenceId = e.Id });
+            UpdateCollection(user.UserRestrictions, model.Restrictions, e => new UserRestriction { UserId = user.Id, RestrictionId = e.Id });
 
             _context.Users.Update(user);
             await _context.SaveChangesAsync();
 
-            PublicUser pu = new PublicUser
-            {
-                Name = user.Name,
-                Email = user.Email,
-                Birthdate = user.Birthdate,
-                Height = user.Height,
-                Weight = user.Weight,
-                Preferences = model.Preferences
-            };
+            return await PublicUser.CreatePublicUser(user, model, _context.Preferences.ToListAsync(), _context.Restrictions.ToListAsync());
+        }
 
-            return pu;
+        private void UpdateCollection<T>(ICollection<T> collection, List<SelectionObject> list, Func<SelectionObject, T> createElement) 
+        {
+            collection.Clear();
+            foreach(SelectionObject e in list)
+            {
+                if (!e.IsSelected) continue;
+
+                collection.Add(createElement(e));
+            }    
         }
     }
-
 }
 
 
---
+++ backend/Models/Data Transfer Objects/PublicUser.cs
@@ -0,0 +1,51 @@
+﻿using backend.Models.Preferences;
+using backend.Models.Restrictions;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend.Models
+{
+    public class PublicUser
+    {
+        public string Id { get; set; }
+        public string? Picture { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public DateOnly Birthdate { get; set; }
+        public Boolean Notifications { get; set; }
+        public float Height { get; set; }
+        public float Weight { get; set; }
+        public List<SelectionObject> Preferences { get; set; } = new();
+        public List<SelectionObject> Restrictions { get; set; } = new();
+
+        public static async Task<PublicUser> CreatePublicUser(User user, PublicUser? model, Task<List<Preference>> listPreferences, Task<List<Restriction>> listRestrictions)
+        {
+            List<SelectionObject> preferences = (model != null) ? model.Preferences : await GetListAsync(listPreferences, p => p.Id, p => p.Name, id => user.UserPreferences.Any(up => up.PreferenceId == id));
+            List<SelectionObject> restrictions = (model != null) ? model.Restrictions : await GetListAsync(listRestrictions, r => r.Id, r => r.Name, id => user.UserRestrictions.Any(up => up.RestrictionId == id));
+
+            return new PublicUser
+            {
+                Picture = user.Picture,
+                Name = user.Name,
+                Email = user.Email,
+                Birthdate = user.Birthdate,
+                Notifications = user.Notifications,
+                Height = user.Height,
+                Weight = user.Weight,
+                Preferences = preferences,
+                Restrictions = restrictions,
+            };
+        }
+
+        private static async Task<List<SelectionObject>> GetListAsync<T>(Task<List<T>> task, Func<T, int> idSelector, Func<T, string> nameSelector, Func<int, bool> isSelectedLogic)
+        {
+            var result = await task;
+
+            return result.Select(e => new SelectionObject
+            {
+                Id = idSelector(e),
+                Name = nameSelector(e),
+                IsSelected = isSelectedLogic(idSelector(e)),
+            }).ToList();
+        }
+    }
+}
