3c7b72b9dfdc2ad2ac5a785ebad2b6f27e7e43df
Luís Martins <202100239@estudantes.ips.pt>
2025-03-06 20:13:07 +0000

    Preferences and Restrictions now are searchable

--- icare.client/src/app/profile/profile.component.ts
+++ icare.client/src/app/profile/profile.component.ts
@@ -33,9 +33,6 @@ export class ProfileComponent implements OnInit {
     picture: '', name: 'Loading', email: '...', birthdate: "2000-01-01", notifications: true, height: 0, weight: 0, preferences: new Set(), restrictions: new Set(), categories: new Set() };
   public todayDate: string;
 
-  public availablePreferences: Set<string> = new Set();
-  public availableRestrictions: Set<string> = new Set();
-
   constructor(private router: Router, private service: UsersService) {
     this.todayDate = new Date().toISOString().split('T')[0];
   }
@@ -48,57 +45,61 @@ export class ProfileComponent implements OnInit {
   }
 
   /**
-   * Adds a new preference to the user's profile and removes it from the available preferences list.
+   * Adds a new preference to the user's profile and removes it from the categories list.
    * @param {Event} event - The event triggered when selecting a preference.
    */
   addPreference(event: Event) {
     const target = event.target as HTMLSelectElement;
     const preference = target.value;
-    if (!preference) return;
+    if (!preference || this.user.preferences.has(preference)) {
+      target.value = '';
+      return;
+    }
 
     this.user.preferences.add(preference);
-    this.availablePreferences.delete(preference);
-
+    this.user.categories.delete(preference);
     target.value = "";
   }
 
   /**
-   * Removes a preference from the user's profile and adds it back to the available preferences list.
+   * Removes a preference from the user's profile and adds it back to the categories list.
    * @param {string} preference - The preference to remove.
    */
   removePreference(preference: string) {
     this.user.preferences.delete(preference);
-    this.availablePreferences.add(preference);
+    this.user.categories.add(preference);
   }
 
   /**
-   * Adds a new dietary restriction to the user's profile and removes it from the available restrictions list.
+   * Adds a new dietary restriction to the user's profile and removes it from the categories list.
    * @param {Event} event - The event triggered when selecting a restriction.
    */
   addRestriction(event: Event) {
     const target = event.target as HTMLSelectElement;
     const restriction = target.value;
-    if (!restriction) return;
+    if (!restriction || this.user.restrictions.has(restriction)) {
+      target.value = "";
+      return;
+    }
 
     this.user.restrictions.add(restriction);
-    this.availableRestrictions.delete(restriction);
-
+    this.user.categories.delete(restriction);
     target.value = "";
   }
 
   /**
-   * Removes a dietary restriction from the user's profile and adds it back to the available restrictions list.
+   * Removes a dietary restriction from the user's profile and adds it back to the categories list.
    * @param {string} restriction - The restriction to remove.
    */
   removeRestriction(restriction: string) {
     this.user.restrictions.delete(restriction);
-    this.availableRestrictions.add(restriction);
+    this.user.categories.add(restriction);
   }
 
   /**
    * Retrieves the user's profile data from the backend service.
    * If the birthdate is missing, it defaults to `"2000-01-01"`.
-   * It also populates the available preferences and restrictions.
+   * It also populates the preferences and restrictions.
    */
   getUser() {
     this.service.getUser().subscribe(
@@ -107,8 +108,7 @@ export class ProfileComponent implements OnInit {
         this.user = { ...result, birthdate, categories: new Set(result.categories), preferences: new Set(result.preferences), restrictions: new Set(result.restrictions) };
 
         for (const c of this.user.categories) {
-          if (!this.user.preferences.has(c)) this.availablePreferences.add(c);
-          if (!this.user.restrictions.has(c)) this.availableRestrictions.add(c);
+          if (this.user.preferences.has(c) || this.user.restrictions.has(c)) this.user.categories.delete(c);
         }
       },
       (error) => {
