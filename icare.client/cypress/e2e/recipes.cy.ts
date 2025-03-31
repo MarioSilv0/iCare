ab43b0c568020d496631876d0797f94dbfb01cbc
João Pedro <irus0ubusiness@gmail.com>
2025-03-31 18:40:28 +0100

    feat: test fav/unfav recipe

--- icare.client/cypress/e2e/recipes.cy.ts
+++ icare.client/cypress/e2e/recipes.cy.ts
@@ -8,8 +8,34 @@ describe("Recipes", () => {
     cy.get('[data-testid="recipes-page"]').should('exist')
   });
 
-  // add/remove filters
-  // search by keyword
-  // favorite/unfavorite recipe
-  // check specified recipe
+  it("should allow the user to add and remove filters", () => { })
+
+  it("should disable filters that the user cannot apply", () => { })
+
+  it("should allow the user to search recipes by given words", () => { })
+
+  it("should allow the user to mark a given recipe as favorite or unfavorite a recipe", () => {
+    // Select the first recipe article
+    cy.get('[data-testid="recipe"]').first().as("firstRecipe");
+
+    // Click the favorite button inside the first recipe
+    cy.get("@firstRecipe").find('[data-testid="bt-toggle-favorite"]').click();
+
+    // Check if the solid heart (favorited) is displayed
+    cy.get("@firstRecipe")
+      .find('[data-testid="bt-toggle-favorite"] img')
+      .should("have.attr", "src", "../../assets/svgs/heart-solid.svg");
+
+    // Click again to unfavorite
+    cy.get("@firstRecipe").find('[data-testid="bt-toggle-favorite"]').click();
+
+    // Check if the regular heart (unfavorited) is displayed
+    cy.get("@firstRecipe")
+      .find('[data-testid="bt-toggle-favorite"] img')
+      .should("have.attr", "src", "../../assets/svgs/heart-regular.svg");
+  })
+
+  it("should allow the user to check the details of a given recipe", () => {
+
+  })
 });
