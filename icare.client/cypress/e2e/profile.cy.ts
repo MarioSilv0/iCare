57a07135454deee1fd639ae9f074cd92eb83f694
João Pedro <irus0ubusiness@gmail.com>
2025-04-02 19:55:09 +0100

    feat: profile tests

--- icare.client/cypress/e2e/profile.cy.ts
+++ icare.client/cypress/e2e/profile.cy.ts
@@ -8,83 +8,164 @@ describe("Profile", () => {
     cy.get('[data-testid="profile-page"]').should('exist')
   });
 
-  it("showld allow the user to add a profile picture", () => {
-    cy.get('[data-testid="profile-image"]').should('exist')
+  //it("should allow the user to add a profile picture", () => {
+  //  cy.get('[data-testid="profile-image"]').should('exist')
 
-    cy.get('[data-testid="profile-image"]', { includeShadowDom: true })
-      .get('[data-testid="image"]')
-      .should('exist') 
-      .attachFile("cat-with-glasses.jpg", { force: true })
+  //  cy.get('[data-testid="profile-image"]', { includeShadowDom: true })
+  //    .get('[data-testid="image"]')
+  //    .should('exist') 
+  //    .attachFile("cat-with-glasses.jpg", { force: true })
 
     
-    cy.get('[data-testid="profile-image"]', { includeShadowDom: true })
-      .find('img')
-      .should("have.attr", "src")
-      .and("include", "cat-with-glasses.jpg");
+  //  cy.get('[data-testid="profile-image"]', { includeShadowDom: true })
+  //    .find('img')
+  //    .should("have.attr", "src")
+  //    .and("include", "cat-with-glasses.jpg");
    
+  //})
+
+  it("should allow the user to change his name", () => {
+    cy.intercept('PUT', `https://127.0.0.1:4200/api/User`, {
+      statusCode: 200,
+    }).as('updateUser');
+
+    cy.get("[data-testid='name-input'] input")
+      .clear()
+      .type("New Name");
+
+    cy.get("[data-testid='submit-input']").click();
+
+    cy.wait('@updateUser').its('response.statusCode').should('eq', 200);
+  });
+
+  it("should allow the user to change his email", () => {
+    cy.intercept('PUT', `https://127.0.0.1:4200/api/User`, {
+      statusCode: 200,
+    }).as('updateUser');
+
+    cy.get("[data-testid='email-input'] input")
+      .clear()
+      .type("john@doe.com");
+
+    cy.get("[data-testid='submit-input']").click();
+
+    cy.wait('@updateUser').its('response.statusCode').should('eq', 200);
   })
 
-  it("showld allow the user to change his name", () => {
-    cy.get("#name").clear().type("New Name");
-    cy.get(".submit-btn").click();
-    cy.get("#name").should("have.value", "New Name");
+  it("should allow the user to change his birthdate", () => {
+    cy.intercept('PUT', `https://127.0.0.1:4200/api/User`, {
+      statusCode: 200,
+    }).as('updateUser');
+
+    cy.get("[data-testid='birthday-input'] input")
+      .clear()
+      .type("2001-04-23");
+
+    cy.get("[data-testid='submit-input']").click();
+
+    cy.wait('@updateUser').its('response.statusCode').should('eq', 200);
   })
 
-  it("showld allow the user to change his email", () => {
-    cy.get("#email").clear().type("newemail@example.com");
-    cy.get(".submit-btn").click();
-    cy.get("#email").should("have.value", "newemail@example.com");
+  it("should allow the user to change his height", () => {
+    cy.intercept('PUT', `https://127.0.0.1:4200/api/User`, {
+      statusCode: 200,
+    }).as('updateUser');
+
+    cy.get("[data-testid='height-input'] input")
+      .clear()
+      .type("1.53");
+
+    cy.get("[data-testid='submit-input']").click();
+
+    cy.wait('@updateUser').its('response.statusCode').should('eq', 200);
   })
 
-  it("showld allow the user to change his birthdate", () => {
-    cy.get("#birthdate").clear().type("2000-01-01");
-    cy.get(".submit-btn").click();
-    cy.get("#birthdate").should("have.value", "2000-01-01");
+  it("should allow the user to change his weigth", () => {
+    cy.intercept('PUT', `https://127.0.0.1:4200/api/User`, {
+      statusCode: 200,
+    }).as('updateUser');
+
+    cy.get("[data-testid='weight-input'] input")
+      .clear()
+      .type("80");
+
+    cy.get("[data-testid='submit-input']").click();
+
+    cy.wait('@updateUser').its('response.statusCode').should('eq', 200);
   })
 
-  it("showld allow the user to change his height", () => {
-    cy.get("#height").clear().type("1.75");
-    cy.get(".submit-btn").click();
-    cy.get("#height").should("have.value", "1.75");
+  it("should allow the user to change his gender", () => {
+    cy.intercept('PUT', `https://127.0.0.1:4200/api/User`, {
+      statusCode: 200,
+    }).as('updateUser');
+
+    cy.get("[data-testid='gender-input']")
+      .select("Feminino")
+
+    cy.get("[data-testid='submit-input']").click();
+
+    cy.wait('@updateUser').its('response.statusCode').should('eq', 200);
   })
 
-  it("showld allow the user to change his weigth", () => {
-    cy.get("#weight").clear().type("70");
-    cy.get(".submit-btn").click();
-    cy.get("#weight").should("have.value", "70");
+  it("should allow the user to change his activity level", () => {
+    cy.intercept('PUT', `https://127.0.0.1:4200/api/User`, {
+      statusCode: 200,
+    }).as('updateUser');
+
+    cy.get("[data-testid='activity-input']")
+      .select("Sedentário")
+      
+    cy.get("[data-testid='submit-input']").click();
+
+    cy.wait('@updateUser').its('response.statusCode').should('eq', 200);
   })
 
-  it("showld allow the user to change his gender", () => {
-    cy.get("#genero").select("Masculino");
-    cy.get(".submit-btn").click();
-    cy.get("#genero").should("have.value", "Masculino");
+  it("should allow the user to turn on/off notifications", () => {
+    cy.intercept('PUT', `https://127.0.0.1:4200/api/User`, {
+      statusCode: 200,
+    }).as('updateUser');
+
+    cy.get("[data-testid='notifications-input'] input")
+      .should("not.be.checked")
+      .click()
+      .should("be.checked")
+
+    cy.get("[data-testid='submit-input']").click();
+
+    cy.wait('@updateUser').its('response.statusCode').should('eq', 200);
   })
 
-  it("showld allow the user to change his activity level", () => {
-    cy.get("#activityLevel").select("Levemente Ativo");
-    cy.get(".submit-btn").click();
-    cy.get("#activityLevel").should("have.value", "Levemente Ativo");
+  it("should allow the user to change his preferences", () => {
+    cy.intercept('PUT', `https://127.0.0.1:4200/api/User`, {
+      statusCode: 200,
+    }).as('updateUser');
+
+    cy.get("[data-testid='preferences-input'] datalist")
+      .find("option")
+      .first()
+      .click({ force: true })
+
+    cy.get("[data-testid='submit-input']").click();
+
+    cy.wait('@updateUser').its('response.statusCode').should('eq', 200);
   })
 
-  it("showld allow the user to turn on/off notifications", () => {
-    cy.get("#notifications").click();
-    cy.get(".submit-btn").click();
-    cy.get("#notifications").should("be.checked");
+  it("should allow the user to change his restritions", () => {
+    cy.intercept('PUT', `https://127.0.0.1:4200/api/User`, {
+      statusCode: 200,
+    }).as('updateUser');
+
+    cy.get("[data-testid='restrictions-input'] datalist")
+      .find("option")
+      .first()
+      .click({ force: true })
+
+    cy.get("[data-testid='submit-input']").click();
+
+    cy.wait('@updateUser').its('response.statusCode').should('eq', 200);
   })
 
-  it("showld allow the user to change his preferences", () => {
-    cy.get("#preferencesList").type("Bom{enter}");
-    cy.get(".submit-btn").click();
-    cy.get("#preferencesList").should("contain", "Bom");
-  })
-
-  it("showld allow the user to change his restritions", () => {
-    cy.get("#restrictionsList").type("Mau{enter}");
-    cy.get(".submit-btn").click();
-    cy.get("#restrictionsList").should("contain", "Mau");
-  })
-
-  it("showld take the user to the 'change password' page", () => {
+  it("should take the user to the 'change password' page", () => {
     cy.get(".change-password").click();
     cy.url().should('include', '/change-password');
   })
